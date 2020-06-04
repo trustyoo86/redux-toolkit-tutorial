c2197fb93343679ae2134c2d4ba41f4e09ddd24c
kernyoo <trustyoo86@gmail.com>
2020-06-05 00:37:34 +0900

    feat(RT-1): Add useSelector

--- src/counter.js
+++ src/counter.js
@@ -1,17 +1,29 @@
 'use strict';
 
-import { createAction, createReducer, configureStore } from '@reduxjs/toolkit';
+import { createAction, createReducer, createSlice, configureStore } from '@reduxjs/toolkit';
 
 const increment = createAction('INCREMENT');
 const decrement = createAction('DECREMENT');
 
-const counter = createReducer(0, {
+// create reducer
+/* eslint-disable */
+const crCounter = createReducer(0, {
   [increment]: state => state + 1,
   [decrement]: state => state - 1,
 });
 
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    [increment]: state => state + 1,
+    [decrement]: state => state - 1,
+  },
+});
+/* eslint-enable */
+
 const store = configureStore({
-  reducer: counter,
+  reducer: counterSlice.reducer,
 });
 
 export default store;
